d5b71c66fc325d3056f316c77ac95599517cc977
zxh0803 <963795293@qq.com>
2017-11-24 17:11:49 +0800

    flask study first

---
+++ app/views.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+from app import app
+from flask import render_template,flash,redirect
+from .forms import LoginForm
+@app.route('/')
+@app.route('/index')
+
+def index():
+    #return "Hello,World!"
+    user = {'nickname':'Miguel'}
+    posts = [
+        {
+            'author':{'nickname':'John'},
+            'body':'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    # return '''
+    # <html>
+    #     <head>
+    #         <title> Home Page</title>
+    #     </head>
+    #     <body>
+    #         <h1>Hello,'''+user['nickname']+'''<h1>
+    #     </body>
+    # </html>
+    # '''
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers = app.config['OPENID_PROVIDERS'])
