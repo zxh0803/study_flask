c89284e8b847bac361d067e0c38572e3d0148298
zxh0803 <963795293@qq.com>
2017-11-27 13:11:41 +0800

    day2 flask

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,21 @@
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import current_user
+from app import views,models
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-from app import views,models
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app,os.path.join(basedir,'tmp'))
+lm.login_view = 'login'
+
+@app.before_request
+def before_request():
+    g.user = current_user
